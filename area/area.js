2b4575bfd4376fbd9bad0903ac35b71b08b904d4
khushijohri2001 <khushijohri2001@gmail.com>
2021-09-23 00:21:19 +0530

    commit: fix error negative

--- area/area.js
+++ area/area.js
@@ -6,9 +6,14 @@ var outputMessage= document.querySelector("#output");
 calculateBtn.addEventListener('click', calculateArea);
 
 function calculateArea(){
+    if (baseValue.value >= 0 && heightValue.value >= 0){
     var area= areaOfTriangle(Number(baseValue.value), Number(heightValue.value));
    
     outputMessage.innerText= "The Area of Triangle is: " + area;
+}
+else {
+    outputMessage.innerText= "Error: Please Enter a positive value ";
+}
 }
 
 function areaOfTriangle(b, h){
